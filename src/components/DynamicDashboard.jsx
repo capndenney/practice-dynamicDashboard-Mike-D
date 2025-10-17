270d5b565f22fe8f1e70b8b74d5aebabbbc2fee7
Mike Denney <mike.a.denney@gmail.com>
2025-10-16 21:21:25 -0500

    working on list items

--- src/components/DynamicDashboard.jsx
+++ src/components/DynamicDashboard.jsx
@@ -1,5 +1,9 @@
+import sampleData from "../sampleData/sampleData"
+
 const DynamicDashboard = () => {
-    
+    <ul>
+        <li>sampleData[0]</li>
+    </ul>
 }
 
 export default DynamicDashboard
